5feade77c59f01a0738ade3857671758e6be1daf
flopown <thijsbenjamin@live.be>
2019-11-29 11:01:25 +0100

    Added calls to a logger when you are recording.
    The triangle and square key on the controller are assigned to the starting and stopping of recording. When we are recording we pass the joystick inputs and the wheel speeds to the logger, the logger logs these as tabular data in a csv file. We also pass the current view of the robot, this gets saved as seperate jpg images.

--- joystick.py
+++ joystick.py
@@ -5,6 +5,7 @@
     It allows for more control than the original script
 """
 from duckietown import Duckietown
+from logger import Logger
 
 import pyglet
 import cv2 as cv
@@ -17,62 +18,41 @@ import argparse
 
 parser = argparse.ArgumentParser(description='This script allows you to control a robot through a joystick in a gym environement')
 # parser.add_argument('--map-name', default='udem1', help='the name of the map')
-# parser.add_argument('--map-name', default='4way', help='the name of the map')
+parser.add_argument('--map-name', default='4way', help='the name of the map')
 # parser.add_argument('--map-name', default='loop_dyn_duckiebots', help='the name of the map')
 # parser.add_argument('--map-name', default='regress_4way_adam', help='the name of the map')
-parser.add_argument('--map-name', default='zigzag_dists', help='the name of the map')
+# parser.add_argument('--map-name', default='zigzag_dists', help='the name of the map')
 
 # The store_true option automatically creates a default value of False.
 parser.add_argument('--distortion', action='store_true', help='enable fish-eye effect on camera (bool)')
 parser.add_argument('--draw-curve', action='store_true', help='draw the lane following curve')
-parser.add_argument('--logging-location', default='recording.log', help='the name of a log file (recording of actions)')
+parser.add_argument('--logging-location', default='recording', help='the name of a log file (recording of actions)')
 args = parser.parse_args()
 
 # Create the duckietown environment
 env = Duckietown(
-    map_name = args.map_name,
-    distortion = args.distortion,
-    draw_curve = args.draw_curve
+    map_name=args.map_name,
+    distortion=args.distortion,
+    draw_curve=args.draw_curve
 )
 
 env.reset()
 env.render()
 
-# Used for computer vision
-image = env.render('rgb_array')
+# Are we recording?
+recording = False
 
-# Not sure what dt stands for lol
-def update(dt):
-    """
-        This method is called every frame to update the screen.
-        This method handles the movement of the duckiebot.
-    """
+# The logger
+logger = Logger(args.logging_location)
+
+
+def read_joystick():
+    global joystick
+
     # left-right on a joystick
     x = round(joystick.x, 2)
     # up-down on a joystick
     y = -round(joystick.y, 2)
-
-    # A truth table for every button (bool)
-    # This is for a Dualshock 4 controller (PS4)
-    keys = {
-        "cross": joystick.buttons[0],
-        "circle": joystick.buttons[1],
-        "triangle": joystick.buttons[2],
-        "square": joystick.buttons[3],
-
-        "L1": joystick.buttons[4],
-        "R1": joystick.buttons[5],
-        "L2": joystick.buttons[6],
-        "R2": joystick.buttons[7],
-
-        "share": joystick.buttons[8],
-        "options": joystick.buttons[9],
-        "power": joystick.buttons[10],
-
-        # These are actual presses on the joysticks, not all controllers have this
-        # "joy_left": joystick.buttons[11],
-        # "joy_right": joystick.buttons[12]
-    }
 
     # Make sure that we only use inputs that are actually usefull
     x, y = x if abs(x) > 0.01 else 0, y if abs(y) > 0.01 else 0
@@ -91,13 +71,14 @@ def update(dt):
         # We are not trying to move, make sure we don't
         vel_left, vel_right = 0, 0
     else:
-        # Calculate the adjustment for the wheel (left wheel if we try to go left, right if we want to go right)
-        adjustment = -direction * velocity * (x**2)
-
         # Adjust the velocities of both wheels given our adjustment
         # We always want the wheels to move at the speed given by the user unless we are turning
-        vel_left = velocity + adjustment if x < 0 else velocity
-        vel_right = velocity + adjustment if x > 0 else velocity
+        vel_left = (1 - x**2) * velocity if x < 0 else velocity
+        vel_right = (1 - x**2) * velocity if x > 0 else velocity
+
+        # We did them in the wrong order if we're going backwards
+        if direction == -1:
+            vel_left, vel_right = vel_right, vel_left
 
         # Cap the speed at the base_velocity, we don't want the bot to go flying off
         if direction > 0:
@@ -109,9 +90,74 @@ def update(dt):
             vel_left, vel_right = max(-base_velocity, vel_left), max(-base_velocity, vel_right)
             vel_left, vel_right = min(0, vel_left), min(0, vel_right)
 
+    return x, y, vel_left, vel_right
+
+
+@env.unwrapped.window.event
+def on_joybutton_press(_joystick, button):
+    global recording, args
+
+    # A truth table for every button (bool)
+    # This is for a Dual-shock 4 controller (PS4)
+    keys = {
+        "cross": _joystick.buttons[0],
+        "circle": _joystick.buttons[1],
+        "triangle": _joystick.buttons[2],
+        "square": _joystick.buttons[3],
+
+        "L1": _joystick.buttons[4],
+        "R1": _joystick.buttons[5],
+        "L2": _joystick.buttons[6],
+        "R2": _joystick.buttons[7],
+
+        "share": _joystick.buttons[8],
+        "options": _joystick.buttons[9],
+        "power": _joystick.buttons[10],
+
+        # These are actual presses on the joysticks, not all controllers have this
+        # "joy_left": joystick.buttons[11],
+        # "joy_right": joystick.buttons[12]
+    }
+
+    if (keys['triangle'] or button == 2) and not recording:
+        print("Turning on recording mode")
+        print('Location of log:', args.logging_location)
+        recording = True
+    elif (keys['square'] or button == 3) and recording:
+        print("Turning off recording mode")
+        print('Location of log:', args.logging_location)
+        recording = False
+
+
+# Not sure what dt stands for lol
+def update(dt):
+    """
+        This method is called every frame to update the screen.
+        This method handles the movement of the duckiebot.
+    """
+    global recording, logger
+
+    joy_x, joy_y, vel_left, vel_right = read_joystick()
+
+    image = env.render('rgb_array')
+
+    if recording:
+        timestamp = int(time())
+
+        logger.writecsv('tabular.csv', {
+            'vel_left': vel_left,
+            'vel_right': vel_right,
+            'joy_x': joy_x,
+            'joy_y': joy_y,
+            'timestamp': timestamp
+        })
+
+        logger.writeimg(f'{timestamp}.jpg', image)
+
     image = env.move([vel_left, vel_right])
 
     env.render()
+
 
 def main():
     global joystick
@@ -128,6 +174,7 @@ def main():
     joystick = joysticks[0]
 
     joystick.open()
+    joystick.push_handlers(on_joybutton_press)
 
     cam_angle = env.unwrapped.cam_angle
     cam_angle[0] -= 5
