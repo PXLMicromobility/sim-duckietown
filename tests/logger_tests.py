0c67db006cf7ab6be7cbfa31b66d4a5071e9266f
thizzhead <buzzed.thizz@outlook.com>
2019-12-19 13:29:49 +0100

    added test coverage
    unittest test raport and coverage

--- tests/logger_tests.py
+++ tests/logger_tests.py
@@ -3,28 +3,31 @@ from PIL import Image
 import numpy
 from logger import Logger
 import os
+import xmlrunner
 
 
 class SimulationTester(unittest.TestCase):
 
     def setUp(self):
-        self.sut = Logger("tests/test_images")
+        self.sut = Logger("tests/test")
 
     def test_writeimg(self):
-        img = Image.open("tests/100.jpeg")
+        img = Image.open("tests/test_images/100.jpeg")
         nimg = numpy.array(img)
         
         with self.subTest("creates_new_image"):
-            self.sut.writeimg("test1.jpeg", nimg)
-            self.assertTrue(os.path.exists("tests/test_images/images/test1.jpeg"))
+            self.sut.writeimg("test1", nimg)
+            self.assertTrue(os.path.exists("tests/test/images/test1.jpeg"))
         
         values = ["2323", ["dzadd", "aezd", "qaa"], 10, [232, 2344, 424, 2232], True, [True, True, False, True, False], 10.0, [10.0, 0.1, 2.2, 3.34]]
 
         for v in values:
             with self.subTest(f"raises_type_exception_on_invalid_value_type_{type(v)}"):
                 with self.assertRaises(TypeError):
-                    self.sut.writeimg("test1.jpeg", v)
+                    self.sut.writeimg("test1", v)
         
     def test_writecsv(self):
+        with self.subTest("creates_csv"):
+            self.sut.writecsv("test", {'vel_left': 0.00, 'vel_right': 0.10, 'joy_x': 1.00, 'joy_y': 1.01, 'index': 0})
+            self.assertTrue(os.path.exists("tests/test/test.csv"))
         
-        pass
